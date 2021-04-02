282ce13320e90dcf1d218c6e8bbcc257036f9a50
afifpriandhika <priandhikaa@gmail.com>
2021-04-02 20:15:45 +0700

    menambahkan jetstream

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/registrasi', function () {
-    return view('registrasi');
-});
-
-Route::get('/diskusiBuat', function () {
-    return view('diskusiBuat');
-});
